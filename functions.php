7332bbcd3b07e8428dae6230f711c61674622df3
root <root@theia.atmos.ucla.edu>
2020-08-20 10:01:52 -0700

    Changing sidebar widgets

--- functions.php
+++ functions.php
@@ -7,3 +7,23 @@ function ucla_theme_enqueue_styles() {
         wp_get_theme()->get('Version') // this only works if you have Version in the style header
     );
 }
+
+add_action( 'after_setup_theme', 'remove_parent_widget_init' );
+
+function remove_parent_widget_init()
+{
+  remove_action( 'widgets_init', 'ucla_widgets_init' );
+  // Add Sidebar widget
+  add_action( 'widgets_init', 'ucla_widgets_init_nolist' );
+  function ucla_widgets_init_nolist() {
+      register_sidebar( array(
+        'name' => esc_html__( 'Sidebar Widget Area', 'ucla' ),
+        'id' => 'primary-widget-area',
+        'before_widget' => '<div id="%1$s" class="widget-container %2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+      ) );
+  }
+  
+}
